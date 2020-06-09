4e593dc18decce62462173c71b689bc370c5d860
Francesco Marino <francesco.marino.412@students.uniroma2.eu>
2020-06-09 16:59:04 +0200

    Maven dependency update

---
+++ src/main/java/DataStreamProcessingMain.java
@@ -0,0 +1,9 @@
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class DataStreamProcessingMain {
+
+    public static void main(String[] args) {
+
+        StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+    }
+}
