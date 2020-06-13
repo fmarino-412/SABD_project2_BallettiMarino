ef2a77f9613f24e9c02f7e4401e7454650a90476
Francesco Marino <francesco.marino.412@students.uniroma2.eu>
2020-06-13 18:18:03 +0200

    Kafka topic creation

--- docker-env/start-dockers.sh
+++ docker-env/start-dockers.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 docker-compose up -d
+
+$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic flink_topic
+$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic kafka_streams_topic
--- docker-env/stop-dockers.sh
+++ docker-env/stop-dockers.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+$KAFKA_HOME/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic flink_topic
+$KAFKA_HOME/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic kafka_streams_topic
+
 docker-compose down
