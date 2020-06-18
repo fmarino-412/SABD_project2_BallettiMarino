d2ed01aa232b7c8e7ae829fe70e613b693797a81
Francesco Marino <francesco.marino.412@students.uniroma2.eu>
2020-06-18 16:22:17 +0200

    Kafka for flink output in docker scripts

--- docker-env/stop-dockers.sh
+++ docker-env/stop-dockers.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 $KAFKA_HOME/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic flink-topic
 $KAFKA_HOME/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic kafka-streams-topic
+$KAFKA_HOME/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic flink-streams-output-topic-query1-daily
+$KAFKA_HOME/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic flink-streams-output-topic-query1-weekly
+$KAFKA_HOME/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic flink-streams-output-topic-query1-monthly
+$KAFKA_HOME/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic flink-streams-output-topic-query2-daily
+$KAFKA_HOME/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic flink-streams-output-topic-query2-weekly
+$KAFKA_HOME/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic flink-streams-output-topic-query3-daily
+$KAFKA_HOME/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic flink-streams-output-topic-query3-weekly
 $KAFKA_HOME/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic kafka-streams-output-topic-query1-daily
 $KAFKA_HOME/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic kafka-streams-output-topic-query1-weekly
 $KAFKA_HOME/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic kafka-streams-output-topic-query1-monthly
