614a0498b824e8cddff2a2dc7f974b156461515d
Francesco Marino <francesco.marino.412@students.uniroma2.eu>
2020-06-20 17:31:30 +0200

    Kafka streams processing guarantees

--- src/main/java/kafkastreams_dsp/KafkaStreamsConfig.java
+++ src/main/java/kafkastreams_dsp/KafkaStreamsConfig.java
@@ -8,18 +8,24 @@ import java.util.Properties;
 
 public class KafkaStreamsConfig {
 
+	final static private String PROCESSING_GUARANTEE = "exactly_once";
+
 	public static Properties createStreamProperties() {
 
 		Properties props = new Properties();
 
+		// application id
 		props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka-streams-queries");
+		// client id
 		props.put(StreamsConfig.CLIENT_ID_CONFIG, "kafka-streams-queries-client");
+		// list of brokers
 		props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaClusterConfig.BOOTSTRAP_SERVERS);
+		// processing guarantees
+		props.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, PROCESSING_GUARANTEE);
 
-		props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG,
-				Serdes.Long().getClass().getName());
-		props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG,
-				Serdes.String().getClass().getName());
+		// key and value serdes
+		props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Long().getClass().getName());
+		props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
 
 		return props;
 	}
