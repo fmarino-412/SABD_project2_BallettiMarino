41471539c0dfd46fd96f4aa15426b9af2c35be9a
Francesco Marino <francesco.marino.412@students.uniroma2.eu>
2020-06-23 16:24:57 +0200

    Custom time windows abstract class

--- src/main/java/kafkastreams_dsp/windows/CustomTimeWindows.java
+++ src/main/java/kafkastreams_dsp/windows/CustomTimeWindows.java
@@ -7,17 +7,31 @@ import java.time.Duration;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
+/**
+ * Abstract class representing a time window of custom duration
+ */
 public abstract class CustomTimeWindows extends Windows<TimeWindow> {
 
+	// local zone id
 	protected final ZoneId zoneId;
+	// grace period milliseconds
 	protected final long grace;
 
-
+	/**
+	 * Default constructor
+	 * @param zoneId local zone id
+	 * @param grace grace period milliseconds
+	 */
 	protected CustomTimeWindows(final ZoneId zoneId, final Duration grace) {
 		this.zoneId = zoneId;
 		this.grace = grace.toMillis();
 	}
 
+	/**
+	 * Converts the zoned time to milliseconds
+	 * @param zonedDateTime time with an associated time zone
+	 * @return timestamp in milliseconds
+	 */
 	protected long toEpochMilli(final ZonedDateTime zonedDateTime) {
 		return zonedDateTime.toInstant().toEpochMilli();
 	}
